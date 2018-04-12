25c2fef40a98c89810e40ae9706e6296472c3b7d
Mahmoud Adas <mido3ds>
2018-04-13 01:42:13 +0200

    just need an icon spinner and some icons

--- app/src/main/java/com/practice/mahmoudadas/inventoryapp/UI/AddItemActivity.java
+++ app/src/main/java/com/practice/mahmoudadas/inventoryapp/UI/AddItemActivity.java
@@ -6,16 +6,38 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.practice.mahmoudadas.inventoryapp.Data.InventoryContract;
 import com.practice.mahmoudadas.inventoryapp.Data.Item;
 import com.practice.mahmoudadas.inventoryapp.R;
 
 public class AddItemActivity extends AppCompatActivity {
+    private EditText itemNameET;
+    private EditText suppNameET;
+    private EditText quantityET;
+    private EditText priceET;
+    private Spinner iconSpinner;
+
+    private int imgResourceId;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additem);
+
+        itemNameET = findViewById(R.id.add_itemName);
+        suppNameET = findViewById(R.id.add_suppName);
+        quantityET = findViewById(R.id.add_quantity);
+        priceET = findViewById(R.id.add_price);
+        iconSpinner = findViewById(R.id.add_img);
+
+        setupSpinner();
+    }
+
+    private void setupSpinner() {
+        // TODO
     }
 
     @Override
@@ -28,8 +50,13 @@ public class AddItemActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add:
-                insertItem(createItem());
-                finish();
+                try {
+                    createItem().buy(getContentResolver());
+                } catch (Exception e) {
+                    Toast.makeText(this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                } finally {
+                    finish();
+                }
                 return true;
             case android.R.id.home:
                 finish();
@@ -38,11 +65,9 @@ public class AddItemActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertItem(Item item) {
-        getContentResolver().insert(InventoryContract.ItemsTable.CONTENT_URI, item.toContentValues()); // TODO: check for exceptions
-    }
-
     private Item createItem() {
-        return null; // TODO: create item from data the user added
+        return new Item(itemNameET.getText().toString(), suppNameET.getText().toString(),
+                Integer.valueOf(quantityET.getText().toString()),
+                Float.valueOf(priceET.getText().toString()), imgResourceId);
     }
 }
